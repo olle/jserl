a5480a9fe0e87a3bf7cbb3bfac45d1f551b7674d
Olle Törnström <olle@studiomediatech.com>
2013-05-26 22:08:06 +0200

    Added placeholders for the initial API.

--- js_src/jserl.js
+++ js_src/jserl.js
@@ -2,4 +2,12 @@
     
     var jserl = global['jserl'] = {};
 
+    jserl.spawn = function () {
+	// TODO: Spawn process on server.
+    };
+
+    jserl.processes = function () {
+	return []; // TODO: Return list of processes.
+    };
+
 })(window || {}); // Don't break just yet.
